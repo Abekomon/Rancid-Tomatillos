81a9c1ea405bc7f1de758b4a008ca4505a9cc1a3
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-02-02 15:56:50 -0800

    Set up PosterGrid structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import Header from '../Header/Header'
+import movieData from '../../movieData'
 import './App.css'
+import PosterGrid from '../PosterGrid/PosterGrid'
 
 class App extends Component {
   constructor() {
-    super()
+    super() 
+    this.state = {
+      movieData: movieData.movies
+    }
   }
 
   render() {
     return (
       <main>
         <Header />
+        <PosterGrid movies={this.state.movieData}/>
       </main>
     )
   }
---
+++ src/components/PosterGrid/PosterGrid.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Poster from '../Poster/Poster'
+import './PosterGrid.css'
+
+class PosterGrid extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+
+  }
+}
+
+
+export default PosterGrid
