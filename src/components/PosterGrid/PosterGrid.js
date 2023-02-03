aab31daf6a0f3ddff89faa54b3dc8bb50e656ad1
Max Lange <max@langes.us>
2023-02-03 17:04:56 -0500

    Update PosterGrid to class

--- src/components/PosterGrid/PosterGrid.js
+++ src/components/PosterGrid/PosterGrid.js
@@ -2,21 +2,39 @@ import React, { Component } from 'react'
 import Poster from '../Poster/Poster'
 import './PosterGrid.css'
 
-const PosterGrid = (props) => {
-  const moviePosters = props.movies.map(movie => {
+class PosterGrid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainView: true,
+    }
+  }
+
+  render() {
+    const moviePosters = this.props.movies.map(movie => {
+      return (
+        <Poster 
+          key={movie.id}
+          id={movie.id}
+          title={movie.title} 
+          poster={movie.poster_path} 
+          rating={movie.average_rating} 
+        />
+      )
+    })
+    
     return (
-      <Poster 
-        title={movie.title} 
-        poster={movie.poster_path} 
-        rating={movie.average_rating} 
-      />
+      <> 
+        {this.state.mainView && <div className='posterGrid'> 
+          {moviePosters}
+        </div>}
+
+        {!this.state.mainView && <div>
+          <h2>Hello!</h2>
+        </div>}
+      </>
     )
-  })
-  return (
-    <div className='posterGrid'> 
-      {moviePosters}
-    </div>
-  )
+  }
 }
 
 export default PosterGrid
