a0b5e5f5d630d9dc54eb9a087852af02858b610f
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-02-03 17:08:31 -0800

    Move button outside of info container

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -11,21 +11,23 @@ const Movie = () => {
   })
   
   return(
-    <div className='container'>
-      
-      <img src={movieData.singleMovie.poster_path} alt={movieData.singleMovie.title}/>
-      
-      <section className='info'>
-        <h2>{movieData.singleMovie.title}</h2>
-        <h3 className='tagline'>{movieData.singleMovie.tagline}</h3>
-        <p className='overview'>Overview: {movieData.singleMovie.overview}</p>
-        <p className='release'>Release Date: {movieData.singleMovie.release_date}</p>
-        <p className='money'>Budget: {movieData.singleMovie.budget}</p>
-        <p className='money'>Revenue: {movieData.singleMovie.revenue}</p>
-        <p className='runtime'>Runtime: {movieData.singleMovie.runtime}</p>
-        {genres}
-        <button>Back to home</button>
-      </section>
+    <div className='main-container'>
+      <div className='container'>
+        
+        <img src={movieData.singleMovie.poster_path} alt={movieData.singleMovie.title}/>
+        
+        <section className='info'>
+          <h2>{movieData.singleMovie.title}</h2>
+          <h3 className='tagline'>{movieData.singleMovie.tagline}</h3>
+          <p className='overview'>{movieData.singleMovie.overview}</p>
+          <p className='release'>Released {movieData.singleMovie.release_date}</p>
+          <p className='money'>Budget: {movieData.singleMovie.budget}</p>
+          <p className='money'>Revenue: {movieData.singleMovie.revenue}</p>
+          <p className='runtime'>Runtime: {movieData.singleMovie.runtime}</p>
+          {genres}
+        </section>
+      </div>
+      <button>Back to home</button>
     </div>
   )
 }
