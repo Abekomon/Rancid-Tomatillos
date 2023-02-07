812d9c5ff235ac7d0b25fd166b2c76f381898bc2
Max Lange <max@langes.us>
2023-02-06 19:03:11 -0500

    Add fixture + first test

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Dashboard flows', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { fixture: 'movies.json' })
+    cy.visit('http://localhost:3000/')
   })
+  
+  it('Should display the app title on page load', () => {
+    cy.get('h1')
+    .contains('Rancid Tomatillos')
+  })
+
+  it('Should display movies on page load', () => {
+    
+  })
+
 })
