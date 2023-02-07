12c4b312ef51d1af1c6e7b6be338c5fd5506f78d
Max Lange <max@langes.us>
2023-02-06 22:22:54 -0500

    Add more user path tests + additional fixture

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -10,7 +10,17 @@ describe('Dashboard flows', () => {
   })
 
   it('Should display movies on page load', () => {
-    
+    cy.get('.poster').should('be.visible')
+    cy.get('h3').contains('Black Adam')
+    cy.get('h3').contains('The Woman King')
+    cy.get('p').contains('Rating: 4.0')
   })
 
+  it('Should change views when clicking on a movie, and show additional details', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270', { fixture: 'black-adam.json' })
+    
+    cy.get('.poster:nth-child(1) > img').click()
+    cy.get('.container > img').should('be.visible')
+    cy.get('.info').should('be.visible')
+  })
 })
