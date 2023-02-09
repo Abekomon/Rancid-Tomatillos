5ee18641d592ec963b023aed2ab4bffb34f03330
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-02-08 17:26:26 -0800

    Add route for error handling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,11 @@
+import { Redirect } from "react-router-dom";
+
 export default function fetchData(path) {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`).then(response => response.json()).catch(error => console.log(error))
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`).then(response => {
+    if (response.ok) {
+      return response.json()
+      } else {
+        throw new Error(response.status)
+      }
+    })
 }
---
+++ src/components/Errors/Errors.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Errors = () => {
+  return (
+    <h3>Whoops!</h3>
+  )
+}
+
+export default Errors
