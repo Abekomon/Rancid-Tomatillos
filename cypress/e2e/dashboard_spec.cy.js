35da816c6c273ddf4d04430991e65e1820be672d
Max Lange <max@langes.us>
2023-02-13 12:15:20 -0500

    Refactor tests to include an error flows spec

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -5,33 +5,25 @@ describe('Dashboard flows', () => {
     cy.visit('http://localhost:3000/')
   })
   
-  it('Should display the app title on page load', () => {
+  it('User should see the app title on page load', () => {
     cy.get('h1')
     .contains('Rancid Tomatillos')
   })
 
-  it('Should display movies on page load', () => {
+  it('User should see movies on page load', () => {
     cy.get('[data-cy="436270"]').should('be.visible')
     cy.get('[data-cy="724495"]').should('be.visible')
     cy.get('[data-cy="436270"]').contains('Black Adam')
     cy.get('[data-cy="724495"]').contains('The Woman King')
   })
 
-  it('Should change views when clicking on a movie, and show additional details', () => {
+  it('User should see the page change views when clicking on a movie, and show additional details', () => {
     cy.get('[data-cy="436270"]').click()
     cy.get('[data-cy="movie/436270"]').should('be.visible')
     cy.get('[data-cy="info/436270"]').should('be.visible')
   })
-})
 
-it('User should see an error page if server is not working', () => {
-  cy.visit('http://localhost:3000/')
-  cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { statusCode: 500 })
-  cy.get('[data-cy="error-500"]').should('be.visible')
-})
-
-it('User should see an error page if bad path was entered', () => {
-  cy.visit('http://localhost:3000/')
-  cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { statusCode: 404 })
-  cy.get('[data-cy="error-400"]').should('be.visible')
+  it('User should see a searchbar on page load, but not when looking at an individual moive', () => {
+    
+  })
 })
